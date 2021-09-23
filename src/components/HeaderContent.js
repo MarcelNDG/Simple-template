20dd6f55b70e4d5d94a1378a489224aa5c991258
MarcelNDG <69280401+MarcelNDG@users.noreply.github.com>
2021-09-23 09:54:10 -0300

    Initial commit

---
+++ src/components/HeaderContent.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+
+
+
+const HeaderContent = () => {
+    return (
+        <>
+            <h1>LOGO</h1>
+            <div className='headerWrap'>
+                <ul className='headerBtnList'>
+                    <li className='headerBtn'>Item</li>
+                    <li className='headerBtn'>Item</li>
+                    <li className='headerBtn'>Item</li>
+                    <li className='headerBtn'>Item</li>
+
+                </ul>
+                <div className='headerOptionsWrap'>
+                    <div className='headerBtnList'>
+                        <button>Button</button>
+                    </div>                    
+                </div>
+                
+            </div>
+        </>
+    )
+}
+
+export default HeaderContent
